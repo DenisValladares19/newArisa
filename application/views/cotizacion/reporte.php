02bd1105c929e2c9ff4c33d31bb256528c0e948e
= <=>
2020-01-16 22:14:35 -0600

    .

--- application/views/cotizacion/reporte.php
+++ application/views/cotizacion/reporte.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -26,33 +25,4 @@
         
     </div>
 </body>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="<?php echo base_url("resources/css/bootstrap.min.css")?>">
-    <title>Reporte</title>
-</head>
-<body>
-    <div class="container mt-5">
-        <table class="table">
-            <thead>
-                <th>Descripcion</th>
-                <th>Cantidad</th>
-                <th>Precio</th>
-                <th>Total</th>
-            </thead>
-        </table>
-        <pre>
-        <?php 
-            echo var_dump($desc);
-        ?>
-        </pre>
-        
-    </div>
-</body>
->>>>>>> d3c3d8b6e3259ca80cfffa6f0e00555b09bbc3f3
 </html>
